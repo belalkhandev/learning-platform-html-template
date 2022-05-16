7c2371c7dd1c81b4d8ccdf4cc2e6f6c6103a6280
Belal khan <belalkhan.dev@gmail.com>
2022-05-16 17:27:15 +0600

    add html file

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+$(function () {
+    "use strict";
+
+    //**  On Click Functions Here **//
+    function onClicks() {
+        $('.custom-select').selectpicker({
+            style: "btn-primary rounded-end rounded-3 fw-400 fs-15 ps-3 h-40px",
+            title: "search",
+            width: "fit",
+        });
+
+        $('.menu-btn').click(function (event) {
+            $('.sidebar-content-wrapper aside.sidebar').toggleClass('collapse-sidebar');
+            $('.sidebar-content-wrapper .content').toggleClass('large');
+            $('.offcanvas-overlay').toggleClass('active');
+        });
+
+        $('.offcanvas-overlay').click(function (event) {
+            $('.sidebar-content-wrapper aside.sidebar').toggleClass('collapse-sidebar');
+            $('.sidebar-content-wrapper .content').toggleClass('large');
+            $('.offcanvas-overlay').toggleClass('active');
+        });
+
+        $('#mobileSearch').click(function (event) {
+            $('.mobile-search').addClass('active');
+        });
+
+        $('.close-search').click(function (event) {
+            $('.mobile-search').removeClass('active');
+        });
+    }
+
+    function initializeCarousel() {
+        var swiper = new Swiper(".tags-carousel", {
+            slidesPerView: 'auto',
+            allowTouchMove: false,
+            navigation: {
+                nextEl: ".next-btn",
+                prevEl: ".prev-btn",
+            },
+        });
+    }
+
+    //** Initialize Functions **// 
+    onClicks();
+    initializeCarousel();
+});
